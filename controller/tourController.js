0835e6923eacd5b8ea3557a98ea1df7a7eec456e
Maheshwaran P <maheshwaran.cs17@bitsathy.ac.in>
2021-02-07 08:35:06 +0530

    Added Document, Query, Aggregation middleware

--- controller/tourController.js
+++ controller/tourController.js
@@ -82,7 +82,10 @@ exports.addTour = async (req,res) => {
 exports.updateTour = async (req, res) => {
     try {
         console.log(req.body, req.params.id)
-        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body);
+        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+            new : true,
+            runValidators: true
+        });
         res.status(200).json({
             status: 'success',
             data: {
